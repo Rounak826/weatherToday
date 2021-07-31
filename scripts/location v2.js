ddfbd491a3d49f2aed1530ea6ab75a91ee837012
unknown <r26bisen@gmail.com>
2021-07-31 23:51:10 +0530

    first commit

--- scripts/location v2.js
+++ scripts/location v2.js
@@ -18,26 +18,12 @@ async function getName(position){
 async function getZip(position){
     let c=0;
     let pos= position;
-    let data = await zipRequest(pos);
-    zip=data.data[c].postal_code;
-    
-    while(zip==null){
-        c++;
-       zip= data.data[c].postal_code;
-    }
+    let data = await positionIq(pos);
+    zip=data.address.postcode;
     return await zip;
 }
 
-async function zipRequest(pos){
-    
-    let lat = pos.latitude;
-    let long= pos.longitude;
-    let url ='https://api.positionstack.com/v1/reverse?access_key=3a5e579338377988b9f854b8da1ee39f&query='+lat+','+long+'';
-    
-    let res = await fetch(url);
-    return await res.json();
 
-}
 async function positionIq(pos){
     
     let lat = pos.latitude;
